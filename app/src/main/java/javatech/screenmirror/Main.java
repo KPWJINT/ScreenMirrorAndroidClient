974019971f719b5a86e177d90f85c87ccd69a182
sobczakbartosz <bartoszsobczak95@gmail.com>
2017-06-10 22:34:04 +0200

    Crate dialog box to enter ip

--- app/src/main/java/javatech/screenmirror/Main.java
+++ app/src/main/java/javatech/screenmirror/Main.java
@@ -4,14 +4,18 @@ import android.app.Activity;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,11 +25,33 @@ import butterknife.OnClick;
 
 public class Main extends Activity {
 
-    @BindView(R.id.buttonChangeIP)
-    TextView buttonChangeIP;
+    @OnClick(R.id.buttonChangeIP)
+    public void changeIP(View view) {
+        LayoutInflater layoutInflaterAndroid = LayoutInflater.from(this);
+        View mView = layoutInflaterAndroid.inflate(R.layout.ip_input_dialog_box, null);
+        AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(this);
+        alertDialogBuilderUserInput.setView(mView);
 
-    @BindView(R.id.buttonConnect)
-    Button buttonConnect;
+        final EditText userInputDialogEditText = (EditText) mView.findViewById(R.id.userInputDialog);
+
+        alertDialogBuilderUserInput
+                .setCancelable(false)
+                .setPositiveButton("Send", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogBox, int id) {
+                        // ToDo get user input here
+                    }
+                })
+
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogBox, int id) {
+                                dialogBox.cancel();
+                            }
+                        });
+
+        AlertDialog alertDialogAndroid = alertDialogBuilderUserInput.create();
+        alertDialogAndroid.show();
+    }
 
     @OnClick(R.id.buttonConnect)
     public void connect(View view) {
