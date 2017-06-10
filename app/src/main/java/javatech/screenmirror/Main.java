2358997c652859ec7e51d98408c202b774135a4c
sobczakbartosz <bartoszsobczak95@gmail.com>
2017-06-10 22:09:34 +0200

    Connect button creates new ScreenshotActivityhotActivity

--- app/src/main/java/javatech/screenmirror/Main.java
+++ app/src/main/java/javatech/screenmirror/Main.java
@@ -10,6 +10,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -20,11 +21,24 @@ import butterknife.OnClick;
 
 public class Main extends Activity {
 
+    @BindView(R.id.buttonChangeIP)
+    TextView buttonChangeIP;
+
     @BindView(R.id.buttonConnect)
     Button buttonConnect;
 
-    @BindView(R.id.buttonDisconnect)
-    TextView buttonDisconnect;
+    @OnClick(R.id.buttonConnect)
+    public void connect(View view) {
+        Intent screenshotActivityIntent = new Intent(this, ScreenshotActivity.class);
+
+        Bundle hostBundle = new Bundle();
+        hostBundle.putString("HOST", host);
+        screenshotActivityIntent.putExtras(hostBundle);
+
+        startActivity(screenshotActivityIntent);
+    }
+
+    private String host = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
