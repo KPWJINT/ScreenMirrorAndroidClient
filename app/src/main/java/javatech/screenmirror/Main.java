170b4d65f60bf15d9ae92781e94efaffcb74edcc
sobczakbartosz <bartoszsobczak95@gmail.com>
2017-06-10 22:36:49 +0200

    Main class code cleanup

--- app/src/main/java/javatech/screenmirror/Main.java
+++ app/src/main/java/javatech/screenmirror/Main.java
@@ -2,24 +2,13 @@ package javatech.screenmirror;
 
 import android.app.Activity;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -36,9 +25,9 @@ public class Main extends Activity {
 
         alertDialogBuilderUserInput
                 .setCancelable(false)
-                .setPositiveButton("Send", new DialogInterface.OnClickListener() {
+                .setPositiveButton("Change IP", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialogBox, int id) {
-                        // ToDo get user input here
+                        host = userInputDialogEditText.getText().toString();
                     }
                 })
 
